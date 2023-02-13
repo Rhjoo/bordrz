879539bce4a0da51df7c7d96934bd0097ecfacb9
Ryan Joo <ryanhjoo@gmail.com>
2023-02-12 21:53:20 -0800

    fixed import error by dropping .tsx from Map.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 // import { StatusBar } from 'expo-status-bar';
-import Map from "./components/Map.tsx";
+import Map from "./components/Map";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
