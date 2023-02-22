8ae18198ad85f9c8efe0176f8f8940e8ea3a4f53
Ryan Joo <ryanhjoo@gmail.com>
2023-02-21 23:17:49 -0800

    css for button and list

--- components/Map.tsx
+++ components/Map.tsx
@@ -31,7 +31,7 @@ const Map = () => {
           getLocationAndGeocode();
         }}
         >
-        <Text>Get Your Location</Text>
+        <Text style={styles.text}>Get Your Location</Text>
       </TouchableOpacity>
       {/*TODO: once pressed, the button should be grayed out, maybe some spinning animation? */}
       {/* <Text>{location.latitude} {location.longitude}</Text> */}
@@ -47,15 +47,15 @@ const Map = () => {
     <>
       {console.log(address)}
       
-      <Text>{address[0].city}</Text>
-      <Text>{address[0].postalCode}</Text>
-      <Text>{address[0].subregion}</Text>
-      <Text>{address[0].region}</Text>
-      <Text>{address[0].country}</Text>
+      <Text style={styles.item}>City: {address[0].city}</Text>
+      <Text style={styles.item}>Zip Code: {address[0].postalCode}</Text>
+      <Text style={styles.item}>County: {address[0].subregion}</Text>
+      <Text style={styles.item}>State: {address[0].region}</Text>
+      <Text style={styles.item}>Country: {address[0].country}</Text>
     </>
   ) : (
     <>
-      <Text>Empty list</Text>
+      <Text style={styles.list}>Empty list</Text>
     </>
   )
 
@@ -100,10 +100,37 @@ const Map = () => {
 
 const styles = StyleSheet.create({
   button: {
-    height: 25,
+    height: 40,
+    width: "90%",
     borderWidth: 1,
     borderColor: "black",
     backgroundColor: "#DDDDDD",
+    borderRadius: 10,
+    margin: 7,
+    padding: 2,
+    justifyContent: "center"
+  },
+  text: {
+    textAlign: "center",
+    fontFamily: "Verdana",
+    fontSize: 30,
+    fontWeight: "500"
+  },
+  list: {
+    display: "none"
+  },
+  item: {
+    height: 60,
+    width: "90%",
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "white",
+    borderRadius: 7,
+    margin: 2,
+    padding: 10,
+    textAlign: "center", 
+    fontFamily: "Verdana",
+    fontSize: 25
   },
   map: {
     width: "100%",
